0bfbf4cac99168ea154151f16df425c9f0820d46
GitHub Action <action@github.com>
2022-08-01 17:33:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/marlonsvillarama/nscicd/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/marlonsvillarama/nscicd/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.marlonsvillarama.nscicd.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marlonsvillarama.nscicd.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/marlonsvillarama/nscicd/services/MyProjectService.kt
+++ src/main/kotlin/com/github/marlonsvillarama/nscicd/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.marlonsvillarama.nscicd.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marlonsvillarama.nscicd.MyBundle
 
 class MyProjectService(project: Project) {
 
